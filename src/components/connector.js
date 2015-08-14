d7f649a12244cb8462ae1e66c670accbb379db6d
William Buchwalter <wbuchwalter@gmail.com>
2015-08-13 22:03:10 -0400

    :lipstick:

--- src/components/connector.js
+++ src/components/connector.js
@@ -2,35 +2,39 @@ import isFunction from '../utils/isFunction';
 import shallowEqual from '../utils/shallowEqual';
 import invariant from 'invariant';
 
-export default function Connector(store) {
-	return {
-      connect: (selectors, callback, disableCaching = false) => {
-        if (!Array.isArray(selectors)) {
-          selectors = [selectors];
-        }
-
-        invariant(
-          isFunction(callback),
-          'The callback parameter passed to connect must be a Function. Instead received %s.',
-          typeof selector
-          );
-
-        //Initial update
-        let params = selectors.map(selector => selector(store.getState()));
-        callback(...params);
-
-        let unsubscribe = store.subscribe(() => {
-          let nextParams = selectors.map(selector => selector(store.getState()));
-          if(disableCaching || !shallowEqual(params, nextParams)) {
-           callback(...nextParams);
-           params = nextParams;
-         }
-       });
-
-        return unsubscribe;
-      },
-      getStore() {
-        return store;
+export default function Connector() {
+  let store;
+  return {
+    connect: (selectors, callback, disableCaching = false) => {
+      if (!Array.isArray(selectors)) {
+        selectors = [selectors];
       }
+
+      invariant(
+        isFunction(callback),
+        'The callback parameter passed to connect must be a Function. Instead received %s.',
+        typeof selector
+      );
+
+      //Initial update
+      let params = selectors.map(selector => selector(store.getState()));
+      callback(...params);
+
+      let unsubscribe = store.subscribe(() => {
+        let nextParams = selectors.map(selector => selector(store.getState()));
+        if (disableCaching || !shallowEqual(params, nextParams)) {
+          callback(...nextParams);
+          params = nextParams;
+        }
+      });
+
+      return unsubscribe;
+    },
+    getStore() {
+      return store;
+    },
+    setStore(reduxStore) {
+      store = reduxStore;
     }
+  }
 }
--- src/components/ngRedux.js
+++ src/components/ngRedux.js
@@ -1,11 +1,10 @@
 import Connector from './connector';
 
 export default function ngReduxProvider() {
-  let reduxStore = undefined;
-  this.setReduxStore = store => reduxStore = store;
+	let reduxStore = undefined;
+	this.setReduxStore = store => reduxStore = store;
 
-  this.$get = () => {
-    return Connector(reduxStore);
-  }
+	this.$get = () => {
+		return Connector(reduxStore);
+	}
 }
-
