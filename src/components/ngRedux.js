3d51a6309471c43f12584cdff6526adfbe4a7590
William Buchwalter <wbuchwalter@gmail.com>
2015-08-19 20:09:37 -0400

    Store is now created by ngRedux (allows DI in middleware)

--- src/components/ngRedux.js
+++ src/components/ngRedux.js
@@ -1,10 +1,41 @@
 import Connector from './connector';
+import invariant from 'invariant';
+import isFunction from '../utils/isFunction';
+import {createStore, applyMiddleware} from 'redux';
 
 export default function ngReduxProvider() {
-  let reduxStore = undefined;
-  this.setReduxStore = store => reduxStore = store;
+  let _reducer = undefined;
+  let _middlewares = [];
+  let _storeEnhancer = undefined;
 
-  this.$get = () => {
-    return Connector(reduxStore);
+  this.createStoreWith = (reducer, middlewares, storeEnhancer) => {
+  	  invariant(
+        isFunction(reducer),
+        'The reducer parameter passed to createStoreWith must be a Function. Instead received %s.',
+        typeof selector
+      );
+
+      invariant(
+        !storeEnhancer || isFunction(storeEnhancer),
+        'The storeEnhancer parameter passed to createStoreWith must be a Function. Instead received %s.',
+        typeof selector
+      );
+
+      _reducer = reducer;
+      _storeEnhancer = storeEnhancer || createStore;
+      _middlewares = middlewares;
+  };
+
+  this.$get = ($injector) => {
+  	let resolvedMiddleware = [];
+  	for(let middleware of _middlewares) {
+  		if(_.isString(middleware)) {
+  			resolvedMiddleware.push($injector.get(middleware));
+  		} else {
+  			resolvedMiddleware.push(middleware);
+  		}
+  	}
+
+  	return Connector(applyMiddleware(...resolvedMiddleware)(_storeEnhancer)(_reducer));
   }
 }
