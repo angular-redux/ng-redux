67f6bbb1bfabecc23a814671633ac6f08bc023a4
William Buchwalter <wbuchwalter@gmail.com>
2016-01-16 14:16:55 -0500

    Remove $$phase, fix #51

--- src/components/digestMiddleware.js
+++ src/components/digestMiddleware.js
@@ -1,9 +1,5 @@
 export default function digestMiddleware($rootScope) {
   return store => next => action => {
-    if(!$rootScope.$$phase) {
-      $rootScope.$apply(next(action));
-    } else {
-      next(action);
-    }
+      $rootScope.$evalAsync(next(action));
   };
 }
