982e95e3faf6528b35d38af600ca518838043a6e
Daniel Almaguer <danielalmaguer@gmail.com>
2017-08-15 22:03:43 -0500

    chore(build): Switch to rollup + Update Dependencies
    Updates all the dependencies and changes the build from webpack to
    rollup exposing now multiple formats:
    - umd
    - umd minified
    - cjs
    - es

---
+++ rollup.config.js
@@ -0,0 +1,53 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
+
+const env = process.env.NODE_ENV;
+const config = {
+  entry: 'src/index.js',
+  plugins: [],
+};
+
+if (env === 'es' || env === 'cjs') {
+  config.format = env;
+  config.external = [
+    'invariant',
+    'lodash.curry',
+    'lodash.isarray',
+    'lodash.isfunction',
+    'lodash.isobject',
+    'lodash.isplainobject',
+    'lodash.map',
+    'redux',
+  ];
+  config.plugins.push(
+    babel()
+  )
+}
+
+if (env === 'development' || env === 'production') {
+  config.format = 'umd';
+  config.moduleName = 'NgRedux';
+  config.plugins.push(
+    nodeResolve({
+      jsnext: true,
+    }),
+    commonjs(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(env)
+    })
+  )
+}
+
+if (env === 'production') {
+  config.plugins.push(
+    uglify()
+  )
+}
+
+export default config;
