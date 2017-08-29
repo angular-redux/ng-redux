0f9fe1728fa43b1ab4946ee556d63ca754472421
Joha2n <johann.rakotoharisoa@gmail.com>
2017-08-29 12:44:35 +0200

    Fix: update typings to fit with umd module

--- index.d.ts
+++ index.d.ts
@@ -1,35 +1,38 @@
-export interface Reducer extends Function {
-    (state: any, action: any): any;
+declare namespace ngRedux {
+  export interface Reducer extends Function {
+      (state: any, action: any): any;
+  }
+
+  export interface Dispatch extends Function {
+      (action: any): any;
+  }
+
+  export interface MiddlewareArg {
+      dispatch: Dispatch;
+      getState: Function;
+  }
+
+  export interface Middleware extends Function {
+      (obj: MiddlewareArg): Function;
+  }
+
+  export interface INgRedux {
+      getReducer(): Reducer;
+      replaceReducer(nextReducer: Reducer): void;
+      dispatch(action: any): any;
+      getState(): any;
+      subscribe(listener: Function): Function;
+      connect(
+          mapStateToTarget: (state: any) => Object,
+          mapDispatchToTarget?: Object | ((dispatch: Function) => Object)
+      ): (target: Function | Object) => () => void;
+  }
+
+  export interface INgReduxProvider {
+      createStoreWith(reducer: Reducer, middlewares?: Array<Middleware | string>, storeEnhancers?: Function[], initialState?: any): void;
+  }
 }
 
-export interface Dispatch extends Function {
-    (action: any): any;
-}
-
-export interface MiddlewareArg {
-    dispatch: Dispatch;
-    getState: Function;
-}
-
-export interface Middleware extends Function {
-    (obj: MiddlewareArg): Function;
-}
-
-export interface INgRedux {
-    getReducer(): Reducer;
-    replaceReducer(nextReducer: Reducer): void;
-    dispatch(action: any): any;
-    getState(): any;
-    subscribe(listener: Function): Function;
-    connect(
-        mapStateToTarget: (state: any) => Object,
-        mapDispatchToTarget?: Object | ((dispatch: Function) => Object)
-    ): (target: Function | Object) => () => void;
-}
-
-export interface INgReduxProvider {
-    createStoreWith(reducer: Reducer, middlewares?: Array<Middleware | string>, storeEnhancers?: Function[], initialState?: any): void;
-}
-
-export var ngRedux: string;
-export default ngRedux;
+declare var ngRedux: string;
+export as namespace ngRedux;
+export = ngRedux;
