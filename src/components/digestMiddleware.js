f0453a7cb57a3fe38fc494087c7096b15a31bc1b
jtassin <jtassin@artprice.com>
2017-11-16 14:41:14 +0100

    debounce - little trick to avoid a random "Scope.eval is not a function" error

--- src/components/digestMiddleware.js
+++ src/components/digestMiddleware.js
@@ -1,9 +1,11 @@
 import debounce from 'lodash.debounce';
 
 export default function digestMiddleware($rootScope, debounceConfig) {
-  let debouncedFunction = $rootScope.$evalAsync;
+  let debouncedFunction = (expr) => {
+    $rootScope.$evalAsync(expr);
+  };
   if(debounceConfig && debounceConfig.wait && debounceConfig.wait > 0) {
-    debouncedFunction = debounce($rootScope.$evalAsync, debounceConfig.wait, { maxWait: debounceConfig.maxWait });
+    debouncedFunction = debounce(debouncedFunction, debounceConfig.wait, { maxWait: debounceConfig.maxWait });
   }
   return store => next => action => {
     const res = next(action);
